ec2f5d0f33dae0b8f318a2d5d1d7095f1687ded5
Oscar024 <or.carvajal.ortiz@gmail.com>
2018-05-13 14:32:39 -0700

    ModelBP con error MSE de 7.7e-5 Agregando formula del framingham

--- NNtest.py
+++ NNtest.py
@@ -1,21 +1,26 @@
 from keras.models import load_model
 import numpy as np
-
+import math
 
 model = load_model('modelBP.h5')
 
 edad = 60
 sexo = 0
-BMI = 133
-diastolica = 132
-siastolica = 54
+bmi = 133.3
+dia = 132
+sys = 54
 fuma = 1
 padres =1
-target = 0.99
 
 
 
-X_test = np.array([[edad,sexo,BMI,diastolica,siastolica,fuma,padres]])
+target = 1-math.exp(-math.exp((math.log(4) - (22.949536 + (-0.156412*edad )+( -0.202933*sexo) + (-0.033881*bmi) + (-0.05933*sys) + (-0.128468*dia) + (-0.190731*fuma) +  (-0.166121*padres) + (0.001624*edad*dia))/0.876925)))
+
+risk = target*100
+print(risk)
+
+
+X_test = np.array([[edad,sexo,bmi,dia,sys,fuma,padres]])
 y_test = np.array([[target]])
 print(X_test.shape)
 yp= model.predict(X_test)
